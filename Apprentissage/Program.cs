dfc54566c5f87572d64e9773cf2673eb097d3504
Maxime VERNOUX <mxm.vernoux@gmail.com>
2023-01-17 23:15:37 +0100

    Ajoutez des fichiers projet.

---
+++ Apprentissage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apprentissage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
